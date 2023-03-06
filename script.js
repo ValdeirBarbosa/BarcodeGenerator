0256ef4ed557991499f1aadeedbf9fde7bbccb7d
valdeir.barbosa <valdeir.barbosa@outlook.com>
2023-03-06 06:32:24 -0300

    new fornt

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 
-        
+
 const canvas = document.getElementById("barcode");
 const canvas1 = document.getElementById("barcode1");
 
+const btn = document.querySelector('.container-btn Button')
+
+const barcodeTitle = document.querySelector('#titlulo')
+const barcodeData = document.querySelector('#conteudo')
+const barcodeGallery = document.querySelector(".barcode-gallerycontainer")
 
 
 
 
-JsBarcode("#barcode", "3891", {
-  width: 4,
+let barcodelist = []
+
+
+
+
+barcodeData.addEventListener('change',()=>{
+
+  codigo = barcodeData.value
+
+})
+
+
+
+
+btn.addEventListener('click', (event) => {
+  
+  barcopdeTitulo = barcodeTitle.value
+  barcodeBody = barcodeData.value
+
+  event.preventDefault();
+  facrotyTag(barcodeData.value)
+
+  console.log(typeof(barcodeBody))
+
+})
+
+
+JsBarcode(".barcode","88888888", {
+  width: 2,
   height: 100,
   font: "Arial",
   background: "#fff",
@@ -17,3 +49,36 @@ JsBarcode("#barcode", "3891", {
 });
 
 
+
+function facrotyTag(tituloText) {
+
+  const barcodeCard = document.createElement("div");
+  barcodeCard.className = "barcode-card"
+  
+  const barcodeProfile = document.createElement("div");
+  barcodeProfile.className = "profile"
+  
+  const titulo = document.createElement("p");
+  titulo.innerText=tituloText
+  
+  const svgBarcode = document.createElement("svg");
+  svgBarcode.className="barcode"
+  
+  
+  barcodeCard.appendChild(barcodeProfile)
+  barcodeProfile.appendChild(titulo)
+  barcodeCard.appendChild(svgBarcode)
+
+  let barcodeG = document.querySelector('.barcode')
+  
+  barcodelist.push(barcodeG)
+
+console.log(barcodelist)
+
+
+
+
+
+
+
+}
