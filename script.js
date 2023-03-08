35a44d62bb0b2af95e4e7f29f9cf0749c01f111f
Barbosa, Valdeir <barbov1@medtronic.com>
2023-03-08 18:00:43 -0300

    ajustes e geração de pdf ok

--- script.js
+++ script.js
@@ -9,57 +9,55 @@ const barcodeGallery = document.querySelector('.barcode-gallerycontainer')
 
 
 btnBarcode.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log(titleBarcode.value, String(dataBarcode.value).padStart(8, "0"))
-    barcodeList.innerHTML += ` <div class="data-row">
+  event.preventDefault();
+  console.log(titleBarcode.value, String(dataBarcode.value).padStart(8, "0"))
+  barcodeList.innerHTML += ` <div class="data-row">
     <div class="title">${titleBarcode.value}</div>
     <div class="barcode-data" id=${String(dataBarcode.value).padStart(8, "0")}>${String(dataBarcode.value).padStart(8, "0")}</div>
 </div>`
 
 
 
-    // barcodes.push({
-    //     titulo:String(titleBarcode.value),
-    //     itf:String(dataBarcode.value).padStart(8,"0")
+  // barcodes.push({
+  //     titulo:String(titleBarcode.value),
+  //     itf:String(dataBarcode.value).padStart(8,"0")
 
-    // })
+  // })
 
-    let barcodes = [{
-        titulo: String(titleBarcode.value),
-        itf: String(dataBarcode.value).padStart(8, "0")
-
-    }]
-
-
-    for (let bar of barcodes) {
-        const el = document.createElement("canvas");
-        el.setAttribute("id", bar.titulo.split(' ').join('') + bar.itf);
-
-        const card = document.createElement("div")
-        card.classList.add("barcode-card")
-        const cardProfile = document.createElement("div")
-        cardProfile.classList.add("barcode-profile")
-        const barUserName = document.createElement("h1")
-        barUserName.textContent = bar.titulo
-        
-        card.appendChild(cardProfile)
-        cardProfile.appendChild(barUserName)
-        barcodeGallery.appendChild(card);
-        card.appendChild(el)
-
-
-        JsBarcode(`#${bar.titulo.split(' ').join('')}${bar.itf}`, bar.itf, {
-            width: 3,
-            height: 50,
-            font: "Arial",
-            background: "#f5f5f5",
-            lineColor: "#000",
-            format: "ITF",
-        });
+  let barcodes = [
+    {
+      titulo: String(titleBarcode.value),
+      itf: String(dataBarcode.value).padStart(8, "0")
     }
+  ]
 
 
+  for (let bar of barcodes) {
+    const el = document.createElement("canvas");
+    el.setAttribute("id", bar.titulo.split(' ').join('') + bar.itf);
 
+    const card = document.createElement("div")
+    card.classList.add("barcode-card")
+    const cardProfile = document.createElement("div")
+    cardProfile.classList.add("barcode-profile")
+    const barUserName = document.createElement("h1")
+    barUserName.textContent = bar.titulo
+
+    card.appendChild(cardProfile)
+    cardProfile.appendChild(barUserName)
+    barcodeGallery.appendChild(card);
+    card.appendChild(el)
+
+
+    JsBarcode(`#${bar.titulo.split(' ').join('')}${bar.itf}`, bar.itf, {
+      width: 3,
+      height: 50,
+      font: "Arial",
+      background: "#f5f5f5",
+      lineColor: "#000",
+      format: "ITF",
+    });
+  }
 })
 
 
